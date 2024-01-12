dd55f1b5d5f90c979abd6176752148e3c8889410
Khadim Gueye <89599484+KhadimGueyeKGY@users.noreply.github.com>
2024-01-12 18:05:46 +0000

    V1.0.0

---
+++ script.py
@@ -0,0 +1,7 @@
+i =0 
+
+while i < 1000: 
+    print(str(i)+'\033[92m'+'\tcette instruction est une boucle infinie'+'\033[0m')
+    print(str(i)+'\033[93m'+'\tthis instruction is an infinite loop'+'\033[0m')
+
+    i += 1
